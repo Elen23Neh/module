7fd7f3fb1d934312390a0f4f26563b31e40b377c
Elen23Neh <nehotkoe23@gmail.com>
2018-01-18 22:50:18 +0200

    finish password

--- src/Password.java
+++ src/Password.java
@@ -4,12 +4,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Password {
 
     public static void main(String[] args) throws Exception {
 
-        ArrayList<String> passwords = readPasswords("C:\\Users\\User\\Desktop\\f\\ff.txt");
+        ArrayList<String> passwords = readPasswords("C:\\Users\\Lena\\Desktop\\f\\ff.txt");
         HashMap<String, Integer> popular = new HashMap<String, Integer>();
 
         for (int i = 0; i < passwords.size(); i++) {
@@ -17,8 +18,30 @@ public class Password {
             popular.put(passwords.get(i), count);
         }
 
-        popular.
+        ArrayList<String> top = getTop10(popular);
 
+        for (int i = 1; i < top.size() + 1; i++) {
+            System.out.println((i) + ". " + top.get(i - 1));
+        }
+    }
+
+    private static ArrayList<String> getTop10(HashMap<String, Integer> passwords) {
+        ArrayList<String> res = new ArrayList<String>();
+        String key = "";
+        for (int i = 0; i < 10 || i < passwords.size(); i++) {
+            int max = 0;
+            for (Map.Entry<String, Integer> entry : passwords.entrySet()) {
+                Integer value = entry.getValue();
+                if (value > max) {
+                    max = value;
+                    key = entry.getKey();
+                }
+            }
+            res.add(key);
+            passwords.remove(key);
+        }
+
+        return res;
     }
 
     private static ArrayList<String> readPasswords(String pathname) throws IOException {
